0dd6000997ce7a198a2d8325f7845e1b3ee08515
JohnSergio <42246920+JohnSergio@users.noreply.github.com>
2018-08-12 23:22:12 -0500

    Proyect Create

---
+++ Slaes.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Slaes.Backend.MvcApplication" Language="C#" %>
---
+++ Slaes.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Slaes.Backend.Startup))]
+namespace Slaes.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
